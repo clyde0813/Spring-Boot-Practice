ae4a278bf6675b2208ade159e8cd6c1f342bcc84
drericcity <drericcity@gmail.com>
2025-08-13 14:48:15 +0000

    Rafactoring AppConfig

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.discount.DiscountPolicy;
+import com.example.demo.discount.FixDiscountPolicy;
 import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
@@ -10,11 +12,19 @@ import com.example.demo.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
     
 }
