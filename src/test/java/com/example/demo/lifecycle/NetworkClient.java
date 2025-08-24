6404de87e725fb17b00bdc58ad9ab36905789568
drericcity <drericcity@gmail.com>
2025-08-24 10:09:30 +0000

    @Bean initMethod, destroyMethod

--- src/test/java/com/example/demo/lifecycle/NetworkClient.java
+++ src/test/java/com/example/demo/lifecycle/NetworkClient.java
@@ -1,11 +1,6 @@
 package com.example.demo.lifecycle;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,16 +24,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("disconnect : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    
+    public void close() {
+        System.out.println("NetworkClient.close");
         disConnect();
     }
 }
