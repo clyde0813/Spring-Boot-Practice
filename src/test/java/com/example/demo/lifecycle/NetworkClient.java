99022e195b63df7a0889df560c69d6bf875466ba
drericcity <drericcity@gmail.com>
2025-08-24 07:19:20 +0000

    InitializingBean, DisposableBean - Deprecated

---
+++ src/test/java/com/example/demo/lifecycle/NetworkClient.java
@@ -0,0 +1,44 @@
+package com.example.demo.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + ", message = " + message);
+    }
+
+    public void disConnect() {
+        System.out.println("disconnect : " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disConnect();
+    }
+}
