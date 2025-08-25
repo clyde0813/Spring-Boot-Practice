78b3bba46701b366b7a5c7c4755c7fe392a6f1bd
drericcity <drericcity@gmail.com>
2025-08-25 14:57:31 +0000

    @Scope(value = "request") - MyLogger

---
+++ src/main/java/com/example/demo/web/LogDemoService.java
@@ -0,0 +1,20 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.common.MyLogger;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
+    }
+}
