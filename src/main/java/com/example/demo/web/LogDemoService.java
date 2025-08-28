99b076fdf2b691234bb617ac31768bbad3dd6946
drericcity <drericcity@gmail.com>
2025-08-28 14:19:03 +0000

    @Scope - proxyMode

--- src/main/java/com/example/demo/web/LogDemoService.java
+++ src/main/java/com/example/demo/web/LogDemoService.java
@@ -11,10 +11,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
